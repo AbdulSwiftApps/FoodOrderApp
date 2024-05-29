6e2ea17f68a57eadc990dfdacc2a67f876862d95
Abdul <abdul.hassan1@alaskaair.com>
2024-05-28 18:05:10 -0700

    Initial Commit

---
+++ FoodOrderApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodOrderApp
+//
+//  Created by Abdul Hassan on 5/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodOrderApp/FoodOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderAppApp.swift
+//  FoodOrderApp
+//
+//  Created by Abdul Hassan on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
